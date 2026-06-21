250b3165d9e957620c4983c71b4eee84e2b3c88b
SiddMallick <msiddhartha1600@gmail.com>
2026-06-22 01:02:57 +0530

    Test change

--- lambdas/delete-todo-lambda/lambda_function.py
+++ lambdas/delete-todo-lambda/lambda_function.py
@@ -22,8 +22,6 @@ def lambda_handler(event, context):
         )
         
         print(response)
-        print("Test Change")
-
         return {
             'statusCode': 200,
             'headers': {
